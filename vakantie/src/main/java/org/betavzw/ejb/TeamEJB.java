32f038b5da2631c5ed114875ab0bc5b3133d3213
user104 <user104@192.168.23.118>
2015-09-11 15:45:06 +0200

    k

--- vakantie/src/main/java/org/betavzw/ejb/TeamEJB.java
+++ vakantie/src/main/java/org/betavzw/ejb/TeamEJB.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 

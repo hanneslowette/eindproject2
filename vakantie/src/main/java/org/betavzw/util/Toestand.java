313e2bdb2615548a47c6e3139f11b16d97076235
user104 <user104@192.168.23.118>
2015-09-11 15:27:11 +0200

    comments toestand

--- vakantie/src/main/java/org/betavzw/util/Toestand.java
+++ vakantie/src/main/java/org/betavzw/util/Toestand.java
@@ -1,5 +1,31 @@
 package org.betavzw.util;
 
+/**
+ * Duid de toestand aan van de verlof aanvraag
+ * 
+ * @author Hannes Lowette
+ *
+ */
 public enum Toestand {
-	PENDING, ACCEPTED, REJECTED, CANCELED;
+	
+	/**
+	 * De toestand wanneer de aanvraag is gebeurd, maar nog niet is beoordeeld door
+	 * de verantwoordelijke
+	 */
+	PENDING,
+	
+	/**
+	 * De toestand wanneer de verantwoordelijke de aanvraag heeft goedgekeurd
+	 */
+	ACCEPTED,
+	
+	/**
+	 * De toestand wanneer de verantwoordelijke de aanvraagd heeft afgekeurd
+	 */
+	REJECTED,
+	
+	/**
+	 * De toestand wanneer de aanvraag is gestopt door de werknemer
+	 */
+	CANCELED;
 }

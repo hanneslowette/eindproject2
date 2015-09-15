c7d379da6fdb18c214401aec166781b77058091a
user109 <user109@192.168.23.123>
2015-09-15 10:36:04 +0200

    IVerlofAanvraag aangepast.

--- vakantie/src/main/java/org/betavzw/ejb/VerlofAanvraagEJB.java
+++ vakantie/src/main/java/org/betavzw/ejb/VerlofAanvraagEJB.java
@@ -15,7 +15,7 @@ import org.betavzw.util.Toestand;
  */
 
 @Stateful
-public class VerlofAanvraagEJB implements VerlofAanvraagInterface{
+public class VerlofAanvraagEJB {
 
 	@PersistenceContext
 	private EntityManager manager;

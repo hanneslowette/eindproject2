c174bc5ebced17475b05a14827ea1aefb1a95137
user106 <user106@192.168.23.115>
2015-09-15 11:45:48 +0200

    Aanpassingen aan interfaces

--- vakantie/src/main/java/org/betavzw/ejb/ITeam.java
+++ vakantie/src/main/java/org/betavzw/ejb/ITeam.java
@@ -21,13 +21,14 @@ public interface ITeam {
 	 * @return
 	 */
 	public List<Team> getTeams(Filter... filter);
-	
+
 	// voegt een team toe in de database
 	public void voegTeamToe(Team team);
-	
+
+	// verwijderen van team uit database
 	public void verwijderTeam(Team team);
-	
+
+	// wijziging van team doorvioeren ind atabase
 	public void wijzigTeam(Team team);
-	
 
 }
--- vakantie/src/main/java/org/betavzw/ejb/IWerknemer.java
+++ vakantie/src/main/java/org/betavzw/ejb/IWerknemer.java
@@ -14,12 +14,14 @@ public interface IWerknemer {
 	 * @return
 	 */
 	public List<Werknemer> getWerknemers(Filter... filter);
-	
+
 	// voegt een werknemer toe in de database
 	public void voegWerknemerToe(Werknemer werknemer);
-	
+
+	// wijzigen van werknemer doorvoeren ind atabase
 	public void wijzigWerknemer(Werknemer werknemer);
-	
+
+	// verwijderen van werknemer uit database
 	public void verwijderWerknemer(Werknemer werknemer);
 
 }

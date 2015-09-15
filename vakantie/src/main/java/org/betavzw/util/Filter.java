e293145395fc44b91302336219ce3c56ce67c7e0
user104 <user104@192.168.23.118>
2015-09-15 16:22:42 +0200

    kkkkkkkk

--- vakantie/src/main/java/org/betavzw/util/Filter.java
+++ vakantie/src/main/java/org/betavzw/util/Filter.java
@@ -3,7 +3,7 @@ package org.betavzw.util;
 public class Filter {
 
 	/**
-	 * 
+	 * De naam van de variable waar je op wil vergelijken. (Meestal een veld in de klasse waarop gecheckt moet worden)
 	 */
 	private final String column;
 	

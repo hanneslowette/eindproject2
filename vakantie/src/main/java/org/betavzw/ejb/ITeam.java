8a0c681a8fee146188835e3202454e862c0f0144
user106 <user106@192.168.23.115>
2015-09-15 10:19:55 +0200

    Aanpassing ITeam

--- vakantie/src/main/java/org/betavzw/ejb/ITeam.java
+++ vakantie/src/main/java/org/betavzw/ejb/ITeam.java
@@ -3,12 +3,22 @@ package org.betavzw.ejb;
 import java.util.List;
 
 import org.betavzw.entities.Team;
+//import org.betavzw.entities.Werknemer;
+import org.betavzw.util.Filter;
 
 public interface ITeam {
 	// geeft een lijst met teams terug, om te gebruiken in werknemertoevoegen
-	public List<Team> getTeams();
+	//public List<Team> getTeams();
 
 	// geeft een team terug, om te gebruiken in zoekfuncties
-	public Team getTeam(String naam);
+	//public Team getTeam(String naam);
+	
+	/**
+	 * 
+	 * 
+	 * @param filter
+	 * @return
+	 */
+	public List<Team> getTeams(Filter... filter);
 
 }

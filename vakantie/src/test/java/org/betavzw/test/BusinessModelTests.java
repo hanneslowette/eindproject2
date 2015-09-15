7ceac55aa32efe5b3be14e4377b06139008eec20
user107 <user107@192.168.23.117>
2015-09-15 16:58:42 +0200

    BusinessModelTests

---
+++ vakantie/src/test/java/org/betavzw/test/BusinessModelTests.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.betavzw.test;
+
+import static org.junit.Assert.*;
+
+import org.betavzw.entities.Adres;
+import org.betavzw.entities.CollectiefVerlof;
+import org.betavzw.entities.CollectieveSluiting;
+import org.betavzw.entities.Feestdag;
+import org.betavzw.entities.JaarlijksVerlof;
+import org.betavzw.entities.Team;
+import org.betavzw.entities.Werknemer;
+import org.junit.Test;
+
+/**
+ * @author user107
+ *
+ */
+public class BusinessModelTests {
+	private Werknemer werknemer;
+	private Team team;
+	private JaarlijksVerlof jaarlijksVerlof;
+	private CollectieveSluiting collectieveSluiting;
+	private Feestdag feestDag;
+	private CollectiefVerlof collectiefVerlof;
+	
+	public BusinessModelTests(){
+		
+	}
+	@Test
+	public void test() {
+		
+	}
+
+}

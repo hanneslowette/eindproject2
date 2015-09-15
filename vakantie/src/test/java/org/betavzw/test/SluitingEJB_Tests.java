df5d73130a9313fd3541388182d2e3df14dd1b0e
Koen De Voegt <koen@devoegt.be>
2015-09-15 14:38:44 +0200

    SluitingEJB_Tests

---
+++ vakantie/src/test/java/org/betavzw/test/SluitingEJB_Tests.java
@@ -0,0 +1,14 @@
+package org.betavzw.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SluitingEJB_Tests {
+
+	@Test
+	public void testSluitingEJB() {
+		assertTrue("Empty class", false);
+	}
+	
+}

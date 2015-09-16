977556de1e71574e1d3019bc38e5c6c52ec0cb6f
user107 <user107@192.168.23.117>
2015-09-16 14:16:46 +0200

    Hannes zijn login serializable gemaakt

--- vakantie/src/main/java/org/betavzw/view/io/LoginIO.java
+++ vakantie/src/main/java/org/betavzw/view/io/LoginIO.java
@@ -1,11 +1,18 @@
 package org.betavzw.view.io;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 @Named("login")
 @SessionScoped
-public class LoginIO {
+public class LoginIO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	
 

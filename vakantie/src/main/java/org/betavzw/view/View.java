a38f41411c87ae1b52f8d8284156b01dc9860193
user107 <user107@192.168.23.117>
2015-09-16 13:59:16 +0200

    view interface afgewerkt mogelijkheid om te cancellen in verlofgoedkeuren verwijderd

--- vakantie/src/main/java/org/betavzw/view/View.java
+++ vakantie/src/main/java/org/betavzw/view/View.java
@@ -1,30 +1,66 @@
 package org.betavzw.view;
 
 public interface View {
-	
+
+	/**
+	 * Author Brent Courtois, senior java developer specialist architect, &
+	 * otter Hannes
+	 **/
+
 	/**
 	 * De home view
 	 */
 	public static final String HOME = "home";
-
+	/**
+	 * View om een team op te vragen
+	 */
+	public static final String TEAM_OPVRAGEN = "opvragenTeam";
+	/**
+	 * View om een team te kunnen toevoegen
+	 */
+	public static final String TEAM_TOEVOEGEN = "teamToevoegen";
+	/**
+	 * De view waar je werknemers kunt zoeken
+	 */
+	public static final String WERKNEMER_OPVRAGEN = "opvragenWerknemer";
+	/**
+	 * view om een werknemer toe te voegen
+	 */
+	public static final String WERKNEMER_TOEVOEGEN = "werknemerToevoegen";
 	/**
 	 * De view die de werknemer bewerkt
 	 */
-	public static final String WIJZIG_WERKNEMER = "wijzigWerknemer";
-	
+	public static final String WERKNEMER_WIJZIGEN = "wijzigen";
 	/**
-	 * De view die wordt weergegeven als de gebruiker voor het eerst verlof aanvraagt
+	 * View met uw verlofaanvragen
+	 */
+	public static final String VERLOFAANVRAGEN = "verlofaanvragen";
+	/**
+	 * De view die wordt weergegeven als de gebruiker voor het eerst verlof
+	 * aanvraagt
 	 */
 	public static final String PENDING = "verlofpending";
-	
 	/**
-	 * De view die wordt weergegeven als de gebruiker voor het eerst verlof aanvraagt
+	 * De view die wordt weergegeven als de administrator een verlof goedkeurt
 	 */
 	public static final String ACCEPTED = "verlofgoedgekeurd";
-	
 	/**
-	 * De view die wordt weergegeven als de gebruiker voor het eerst verlof aanvraagt
+	 * De view die wordt weergegeven als de gebruiker voor het eerst verlof
+	 * aanvraagt en deze wordt afgekeurd
 	 */
 	public static final String REJECTED = "verlofafgekeurd";
-
+	/**
+	 * De view die wordt weergegeven als de gebruiker voor het eerst verlof
+	 * aanvraagt
+	 */
+	public static final String VERSTUURD = "verlofaanvraagverstuurd";
+	/**
+	 * Hier kan je via personeelsnummer de jaarlijkse verloven van die werknemer
+	 * bekijken
+	 */
+	public static final String JAARLIJKSVERLOF = "beheerjaarlijkseverloven";
+	/**
+	 * view om een verlof goed te keuren/ af te keuren
+	 */
+	public static final String VERLOFGOEDKEUREN = "verlofgoedkeuren";
 }

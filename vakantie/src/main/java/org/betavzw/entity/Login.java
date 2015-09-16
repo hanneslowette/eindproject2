b7d8347db0ac0fee1cc57d6db2923a30d3d8594b
Koen De Voegt <koen@devoegt.be>
2015-09-16 16:03:22 +0200

    Generated getters and setters

--- vakantie/src/main/java/org/betavzw/entity/Login.java
+++ vakantie/src/main/java/org/betavzw/entity/Login.java
@@ -12,4 +12,28 @@ public class Login {
 	@Id
 	private int werknemerId;
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getWerknemerId() {
+		return werknemerId;
+	}
+
+	public void setWerknemerId(int werknemerId) {
+		this.werknemerId = werknemerId;
+	}
+
 }

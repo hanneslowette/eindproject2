645bb9f25fcdb342ae47a9bc0a75450c11eca554
Koen De Voegt <koen@devoegt.be>
2015-09-18 10:09:44 +0200

    tests added

--- vakantie/src/test/java/org/betavzw/test/bean_Tests.java
+++ vakantie/src/test/java/org/betavzw/test/bean_Tests.java
@@ -1,54 +1,103 @@
 package org.betavzw.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import org.betavzw.entity.Werknemer;
 import org.betavzw.view.bean.WerknemerBean;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class bean_Tests {
 
-	EntityManagerFactory emf = Persistence
+	private static EntityManagerFactory emf = Persistence
 			.createEntityManagerFactory("unitName");
 	
-	EntityManager manager = emf.createEntityManager();
+	private static EntityManager manager = emf.createEntityManager();
+	private static WerknemerBean werknemerBean = new WerknemerBean();
+	private static EntityTransaction tx;
 	
-	WerknemerBean werknemerBean = new WerknemerBean();
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+	
+	private Werknemer wn = new Werknemer();
+	
+	private String voornaam = "Koen";
+	private String naam = "De Voegt";
+	private String email = "koen@devoegt.be";
+	private LocalDate date = LocalDate.parse("1980-apr-13", formatter);
+		
+	@BeforeClass
+	public static void masterSetup() {
+		tx = manager.getTransaction();
+		werknemerBean.setEntityManager(manager);
+	}
 	
 	@Before
 	public void setup() {
-		werknemerBean.setEntityManager(manager);
-	} 
+		wn.setVoornaam(voornaam);
+		wn.setNaam(naam);
+		wn.setEmail(email);		
+
+		wn.setGeboortedatum(date);		
+	}
 	
-//	@After
-//	public void setdown() {
-//		emf.close();
-//		manager.close();
-//	} 
+	@AfterClass
+	public static void masterTeardown() {
+		manager.close();
+		emf.close();
+	} 
 	
 	@Test
 	public void test() {
 		assertTrue(manager.isOpen());
 	}
 	
+	@Test
+	public void WerknemerOffer_Test() {
+		
+		tx.begin();
+		werknemerBean.offer(wn);
+		tx.commit();
+		
+		tx.begin();
+		List<Werknemer> list = werknemerBean.get();
+		assertTrue("failure - Werknemer not added", list.contains(wn));
+		Werknemer wn_out = list.get(0);
+		assertEquals("failure - Voornaam doesn't match", wn_out.getVoornaam(), voornaam);
+		assertEquals("failure - Naam doesn't match", wn_out.getNaam(), naam);
+		assertEquals("failure - email doesn't match", wn_out.getEmail(), email);
+		assertEquals("failure - Geboortedatum doesn't match", wn_out.getGeboortedatum(), date);
+		werknemerBean.delete(wn_out);
+		tx.commit();
+	}
 
 	@Test
-	public void WerknemerUpdate_Test() {
-		Werknemer wn = new Werknemer();
-		wn.setNaam("sqdfqsdf");
-		werknemerBean.update(wn);
-//		werknemerBean.
-		List<Werknemer> list = werknemerBean.get();
-		System.out.println(list.size());
-		assertTrue("failure - Werknemer not added", list.contains(wn));
+	public void WerknemerDelete_Test() {
+		
+		tx.begin();
+		werknemerBean.offer(wn);
+		tx.commit();
+		
+		tx.begin();
+		werknemerBean.delete(wn);
+		tx.commit();
+		
+//		tx.begin();
+//		List<Werknemer> list = werknemerBean.get();
+//		assertEquals("failure - Werknemer not deleted", list.size(), 0);
+//		tx.commit();
+		
 	}
 	
 }

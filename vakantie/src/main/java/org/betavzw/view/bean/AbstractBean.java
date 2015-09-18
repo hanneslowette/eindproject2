856142d46b94b775f827fafc12fd3f56e06940e0
user104 <user104@192.168.23.118>
2015-09-18 16:21:31 +0200

    Abstractbean update

--- vakantie/src/main/java/org/betavzw/view/bean/AbstractBean.java
+++ vakantie/src/main/java/org/betavzw/view/bean/AbstractBean.java
@@ -1,6 +1,10 @@
 package org.betavzw.view.bean;
 
+import java.lang.reflect.Field;
+
+import javax.naming.directory.NoSuchAttributeException;
 import javax.persistence.EntityManager;
+import javax.persistence.Id;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
@@ -24,7 +28,57 @@ public abstract class AbstractBean<T> implements Bean<T> {
 
 	@Override
 	@Transactional public void update(T entity) {
-		manager.persist(entity);
+		try {
+			Class<?> c = entity.getClass();
+			
+			/*
+			 * Zoek het primary key veld
+			 */
+			Field id_field = null;
+			for (Field field : c.getDeclaredFields()) {
+				/*
+				 * Maak het veld accessible
+				 */
+				field.setAccessible(true);
+				
+				/*
+				 * Als het veld de annotatie Id heeft, gebruik dit als de primary key tijdens de find
+				 */
+				id_field = field.isAnnotationPresent(Id.class) ? field : id_field;
+			}
+			
+			/*
+			 * Niets meer doen als er geen primary key is gevonden
+			 */
+			if (id_field == null) {
+				throw new NoSuchAttributeException("no unique id found");
+			}
+			
+			/*
+			 * Haal het object uit de database
+			 */
+			Object fetch = manager.find(c, id_field.get(entity));
+			
+			/*
+			 * Kopieer het object
+			 */
+			for (Field field : c.getDeclaredFields()) {
+				if (!field.isAnnotationPresent(Id.class)) {
+					/*
+					 * Maak het veld accessible
+					 */
+					field.setAccessible(true);
+					System.out.println("Copying " + field.getName() + " [" + field.get(entity) + "] to fetched object");
+					
+					/*
+					 * Zet de waarde van het veld in de database entity naar de opgegeven entity
+					 */
+					field.set(fetch, field.get(entity));
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	@Override
--- vakantie/src/main/java/org/betavzw/view/io/TeamBeheerIO.java
+++ vakantie/src/main/java/org/betavzw/view/io/TeamBeheerIO.java
@@ -2,11 +2,18 @@ package org.betavzw.view.io;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.betavzw.entity.Team;
+import org.betavzw.entity.Werknemer;
+import org.betavzw.util.Filter;
+import org.betavzw.view.View;
 import org.betavzw.view.bean.Bean;
 
+@Named("team_beheer")
+@SessionScoped
 public class TeamBeheerIO implements Serializable {
 
 	/**
@@ -16,8 +23,67 @@ public class TeamBeheerIO implements Serializable {
 
 	@Inject private Bean<Team> teams;
 
+	@Inject private Bean<Werknemer> werknemers;
+
+	/**
+	 * Het team dat bewerkt wordt.
+	 */
+	private Team team;
+	
+	/**
+	 * De naam van het team
+	 */
+	private String naam;
+
+	/**
+	 * De id van de verantwoordelijke
+	 */
+	private int verantwoordelijkeId;
+
+	/**
+	 * Opent deze pagina met het gegeven team als het team dat bewerkt wordt
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public String beheer(int id) {
-		return null;
+		this.team = teams.getSingle(new Filter("id", id));
+		return View.TEAM_BEHEER;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String update() {
+		this.team.setNaam(naam);
+		this.team.setTeamverantwoordelijke(werknemers.getSingle(new Filter("personeelsNr", verantwoordelijkeId)));
+		this.teams.update(team);
+		return View.HOME;
+	}
+
+	public String getCode() {
+		return team.getCode();
+	}
+	
+	public String getNaam() {
+		return team.getNaam();
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public void setVerantwoordelijkeId(int id) {
+		this.verantwoordelijkeId = id;
+	}
+
+	public int getVerantwoordelijkeId() {
+		return verantwoordelijkeId;
+	}
+
+	public Team getTeam() {
+		return team;
 	}
 
 }

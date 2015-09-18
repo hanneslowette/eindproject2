112ea258007a423c848e4ae03aeffee5fc8fc6b1
user109 <user109@192.168.23.123>
2015-09-18 12:15:28 +0200

    - Selenium webdriver dependency toegevoegd in pom - TeamToevoegenTest added - VerlofAanvragenTest added - WerknemerToevoegerTest added

---
+++ vakantie/src/test/java/org/betavzw/view/xhtml/test/WerknemerToevoegenTest.java
@@ -0,0 +1,25 @@
+package org.betavzw.view.xhtml.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WerknemerToevoegenTest {
+
+	public static void main(String[] args) {
+
+		// Create a new instance of Firefox Browser
+		WebDriver driver = new FirefoxDriver();
+
+		// Open the URL in firefox browser
+		driver.get("http://localhost:8080/vakantie/faces/werknemerToevoegen.xhtml");
+
+		// Maximize the Browser window
+		driver.manage().window().maximize();
+
+		// Get the current page URL and store the value in variable 'str'
+		String str = driver.getCurrentUrl();
+
+		// Print the value of variable in the console
+		System.out.println("The current URL is " + str);
+	}
+}

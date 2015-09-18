70dafe3545be6fc91826004abd5c9f225fa47fb3
user104 <user104@192.168.23.118>
2015-09-18 16:27:33 +0200

    werknemer UPDATE

--- vakantie/src/main/java/org/betavzw/view/bean/WerknemerBean.java
+++ vakantie/src/main/java/org/betavzw/view/bean/WerknemerBean.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
+import org.betavzw.entity.Adres;
 import org.betavzw.entity.Werknemer;
 import org.betavzw.util.QueryBuilder;
 
@@ -16,9 +18,27 @@ public class WerknemerBean extends AbstractBean<Werknemer> implements Serializab
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * De adres bean
+	 */
+	@Inject private Bean<Adres> adres_bean;
+
 	@Override
 	public List<Werknemer> get(QueryBuilder query) {
 		return query.build(super.getEntityManager(), Werknemer.class).getResultList();
+	}
+
+	@Override
+	public void update(Werknemer entity) {
+		/*
+		 * Eerst het adres persisten
+		 */
+		adres_bean.update(entity.getAdres());
+		
+		/*
+		 * Dan de werknemer
+		 */
+		super.update(entity);
 	}
 
 }

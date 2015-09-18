23286d9b9ba2d18e61824ee5f04be3b849dd7684
Koen De Voegt <koen@devoegt.be>
2015-09-18 12:13:47 +0200

    Tests

--- vakantie/src/test/java/org/betavzw/entity/test/Adres_Tests.java
+++ vakantie/src/test/java/org/betavzw/entity/test/Adres_Tests.java
@@ -1,6 +1,20 @@
-dfgsdqf
-µs
-df
-sdf
-sd
-f
+package org.betavzw.entity.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.betavzw.entity.Adres;
+import org.junit.Test;
+
+public class Adres_Tests {
+	
+	private Adres adres = new Adres();
+	
+	@Test
+	public void Straat_Test() {
+		String straat = "TestStraat";
+		adres.setStraat(straat);
+		assertEquals("failure - Straat doesn't match", straat, adres.getStraat());
+	}
+	
+	
+}

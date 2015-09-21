1a7fb593258096ab448113f34f19686e252390cb
user107 <user107@192.168.23.117>
2015-09-21 13:40:36 +0200

    emailbean

---
+++ vakantie/src/main/java/org/betavzw/view/bean/MailBean.java
@@ -0,0 +1,36 @@
+package org.betavzw.view.bean;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@ApplicationScoped
+public class MailBean {
+	
+	@Resource(lookup = "redmail")
+    private Session mailSession;
+	
+	public void sendmail(String to, String subject, String body) {
+
+		MimeMessage message = new MimeMessage(mailSession);
+        try {
+            message.setFrom(new InternetAddress(mailSession.getProperty("mail.from")));
+            InternetAddress[] address = {new InternetAddress(to)};
+            message.setRecipients(Message.RecipientType.TO, address);
+            message.setSubject(subject);
+            message.setSentDate(new Date());
+            message.setText(body);
+            Transport.send(message);
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+        }
+	}
+
+}

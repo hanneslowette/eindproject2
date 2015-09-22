69ce85524397663135a878e3b562fa907570ffc1
Koen De Voegt <koen@devoegt.be>
2015-09-22 11:53:44 +0200

    first version CollectiefVerlofToevoegenIO

---
+++ vakantie/src/main/java/org/betavzw/view/io/CollectiefVerlofToevoegenIO.java
@@ -0,0 +1,23 @@
+package org.betavzw.view.io;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.betavzw.entity.CollectiefVerlof;
+import org.betavzw.view.bean.Bean;
+
+@Named
+@SessionScoped
+public class CollectiefVerlofToevoegenIO implements Serializable {
+
+	private static final long serialVersionUID = 1857446570390400499L;
+
+	@Inject
+	private Bean<CollectiefVerlof> collectiefVerlof_bean;
+	
+	private CollectiefVerlof collectiefVerlof;
+	
+}

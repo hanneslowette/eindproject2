463cb2c6fea596518eff5668dcadade057818591
user106 <user106@192.168.23.115>
2015-09-23 11:29:40 +0200

    collectiefverlofException

---
+++ vakantie/src/main/java/org/betavzw/util/exceptions/FoutFeestdagToevoegenException.java
@@ -0,0 +1,10 @@
+package org.betavzw.util.exceptions;
+
+public class FoutFeestdagToevoegenException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- vakantie/src/main/java/org/betavzw/view/io/CollectiefVerlofToevoegenIO.java
+++ vakantie/src/main/java/org/betavzw/view/io/CollectiefVerlofToevoegenIO.java
@@ -1,14 +1,21 @@
 package org.betavzw.view.io;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.betavzw.entity.CollectiefVerlof;
 import org.betavzw.entity.CollectieveSluiting;
 import org.betavzw.entity.Feestdag;
+import org.betavzw.util.exceptions.FoutFeestdagToevoegenException;
+import org.betavzw.util.exceptions.GeboortedatumInDeToekomstException;
 import org.betavzw.view.bean.Bean;
 
 @Named
@@ -49,6 +56,31 @@ public class CollectiefVerlofToevoegenIO implements Serializable {
 	public void setCollectiefVerlof_bean(Bean<Feestdag> collectiefVerlof_bean) {
 		this.collectiefVerlof_bean = collectiefVerlof_bean;
 	}
+	
+	
+//	public Date getDatum() {
+//		return werknemer.getGeboortedatum() == null ? null : Date
+//				.from(werknemer.getGeboortedatum().atStartOfDay()
+//						.atZone(ZoneId.systemDefault()).toInstant().plusSeconds(86001));
+//	}
+
+	public void setDatum(Date datum) {
+		try {
+			LocalDate localdate = datum.toInstant()
+					.atZone(ZoneId.systemDefault()).toLocalDate();
+			feestdag.setStartDatum(localdate);  //.setGeboortedatum(localdate);
+		} catch (FoutFeestdagToevoegenException e) {
+			FacesContext
+					.getCurrentInstance()
+					.addMessage(
+							null,
+							new FacesMessage(
+									"werknemer niet aangepast: geboortedatum mag niet in de toekomst liggen",
+									"update geslaagd"));
+		}
+	}
+	
+	
 
 	// public Bean<CollectieveSluiting> getCollectiefVerlof_bean() {
 	// return collectiefVerlof_bean;
